18a850e361c1e8ca677da5881867b10c4e5a97d7
Sofia <sofiarcones@gmail.com>
2023-01-18 18:55:08 +0100

    Commit2

--- HolaDAM/src/holadam/HolaDAM.java
+++ HolaDAM/src/holadam/HolaDAM.java
@@ -16,6 +16,7 @@ public class HolaDAM {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HolaDAM");
+        System.out.println("Somos Sofia Arcones y David Rayaces");
     }
     
 }
